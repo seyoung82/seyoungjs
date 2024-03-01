c94dd5981247cb82591fe147842524093bd7d798
seyoung <seyoung8244@gmail.com>
2024-03-01 17:16:01 +0900

    404에러

---
+++ app/404.tsx
@@ -0,0 +1,5 @@
+export default function Error404(){
+    return <div style={{padding: '200px 0', textAlign:'center', fontSize:30}}>
+            404.....:페이지를 찾을수 없습니다. 
+        </div>
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="bg-white text-black">>
+      <h1>404에요</h1>
+ 
+    </div>
+  );
+}
