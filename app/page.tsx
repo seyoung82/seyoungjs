d1d9dfac122d3e24bedc2c08f7a0d0e0668e0f1e
seyoung <seyoung8244@gmail.com>
2024-03-01 16:45:12 +0900

    오후

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,20 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Image from "next/image";
 import ItemList from "@/src/component/ItemList";
-import { Divider } from "semantic-ui-react";
+import { Divider, Loader } from "semantic-ui-react";
 import Head from "next/head";
 
 
 export default function Home() {
   const [list,setList] = useState([]);
+  const [isLoading,setIsLoading] = useState(true);
   const API_URL = "https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
     function getData(){
         axios.get(API_URL)
         .then(res=>{
             console.log(res.data);
             setList(res.data);
+            setIsLoading(false);
         });
     }
 
@@ -28,8 +30,22 @@ export default function Home() {
     <Head>
       <title>HOME | 세영</title>
     </Head>
-    <Divider/>
-      <ItemList list={list}/>
+
+    {isLoading && (
+      <div style={{padding:"300px 0"}}>
+        <Loader inline="centered" active>
+          Loading
+        </Loader>
+      </div>
+    )}
+
+    {!isLoading && (
+      <>
+        <Divider/>
+        <ItemList list={list}/>
+      </>
+    )}
+    
     </>
   );
 }
--- app/view/[id]/page.tsx
+++ app/view/[id]/page.tsx
@@ -2,14 +2,18 @@
 import Item from "@/src/component/item";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Loader } from "semantic-ui-react";
 export default function View(props:any){
     const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${props.params.id}.json`;
     const [item,setItem] = useState({});
+    const [isLoading,setIsLoading] = useState(true);
     function getData(){
         axios.get(API_URL)
         .then(res=>{
             console.log(res.data);
             setItem(res.data);
+            setIsLoading(false);
+            
         })
     };
 
@@ -20,5 +24,18 @@ export default function View(props:any){
         
     },[props.params.id]);
 
-    return (<Item item={item}/>);
+    return (
+        <>
+        {isLoading ? (
+            <div style={{padding:"300px 0"}}>
+                <Loader inline="centered" active>
+                    Loading
+                </Loader>
+            </div>
+        ) : (
+            <Item item={item}/>
+        )}
+     </>
+        
+    );
 }
