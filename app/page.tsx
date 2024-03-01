df7e6a1fa46deb93b4a6552fc70e98577ed1f695
seyoung <seyoung8244@gmail.com>
2024-03-01 11:09:55 +0900

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+import axios from "axios";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  const API_URL = "https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+    function getData(){
+        axios.get(API_URL)
+        .then(res=>{
+            console.log(res);
+        });
+    }
+
+    useEffect(()=>{
+      getData();
+    },[]);
   return (
     <>
     <h2>WelCome</h2>
