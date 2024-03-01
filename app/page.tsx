19796ae885a9d039657c60ab2b552b95ae7e1e7f
seyoung <seyoung8244@gmail.com>
2024-03-01 12:42:05 +0900

    작업중

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 "use client"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Image from "next/image";
+import ItemList from "@/src/component/ItemList";
+import { Divider } from "semantic-ui-react";
+import Head from "next/head";
 
 
 export default function Home() {
+  const [list,setList] = useState([]);
   const API_URL = "https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
     function getData(){
         axios.get(API_URL)
         .then(res=>{
-            console.log(res);
+            console.log(res.data);
+            setList(res.data);
         });
     }
 
@@ -20,7 +25,11 @@ export default function Home() {
   return (
     <>
     <h2>WelCome</h2>
-      Hello,WEb
+    <Head>
+      <title>HOME | 세영</title>
+    </Head>
+    <Divider/>
+      <ItemList list={list}/>
     </>
   );
 }
